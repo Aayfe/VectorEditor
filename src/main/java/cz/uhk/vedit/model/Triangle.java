74947ead30d8c85b34b1c463e19122ecbfa0f27b
Jakub Didkovský <jakub@didkovsky.cz>
2026-03-19 16:57:53 +0100

    Circle added

--- src/main/java/cz/uhk/vedit/model/Triangle.java
+++ src/main/java/cz/uhk/vedit/model/Triangle.java
@@ -3,10 +3,19 @@ package cz.uhk.vedit.model;
 import java.awt.*;
 
 public class Triangle extends AbstractGraphicObject{
+    protected int a;
+
+    public Triangle(){
+
+    }
+    public Triangle(Point point, Color color, int a) {
+        super(point, color);
+        this.a = a;
+    }
     @Override
     public void draw(Graphics2D g) {
         g.setColor(this.color);
-
+        g.drawLine(point.x, point.y, a );
     }
 
     @Override
